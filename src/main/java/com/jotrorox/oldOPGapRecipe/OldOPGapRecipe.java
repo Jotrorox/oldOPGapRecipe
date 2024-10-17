453576136da9f6c72149f607912384db36f2ff74
Johannes (Jotrorox) Müller <mail@jotrorox.com>
2024-10-17 22:36:29 +0200

    Update version to 1.0 and add custom recipe for enchanted golden apple

--- src/main/java/com/jotrorox/oldOPGapRecipe/OldOPGapRecipe.java
+++ src/main/java/com/jotrorox/oldOPGapRecipe/OldOPGapRecipe.java
@@ -1,17 +1,19 @@
 package com.jotrorox.oldOPGapRecipe;
 
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class OldOPGapRecipe extends JavaPlugin {
-
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
-    }
-
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+        ItemStack gap = new ItemStack(Material.ENCHANTED_GOLDEN_APPLE, 1);
+        ShapedRecipe recipe = new ShapedRecipe(NamespacedKey.fromString("jotrorox.old_op_gap"), gap);
+        recipe.shape("GGG", "GAG", "GGG");
+        recipe.setIngredient('G', Material.GOLD_BLOCK);
+        recipe.setIngredient('A', Material.APPLE);
+        getServer().addRecipe(recipe);
     }
 }
