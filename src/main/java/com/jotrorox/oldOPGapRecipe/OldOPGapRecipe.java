bc4b055b818576e2123cfbabf30cbf01c0cf4fcd
Johannes (Jotrorox) Müller <mail@jotrorox.com>
2024-10-17 22:36:23 +0200

    Add initial project structure and configuration for oldOPGapRecipe plugin

---
+++ src/main/java/com/jotrorox/oldOPGapRecipe/OldOPGapRecipe.java
@@ -0,0 +1,17 @@
+package com.jotrorox.oldOPGapRecipe;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class OldOPGapRecipe extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
